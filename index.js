8af9e602e8d6334e5796c0b37e7c5cb249323f76
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-10 17:12:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
@@ -14,3 +14,5 @@ function deleteFromObjectByKey(object, key, value) {
   delete newObj [key]
   return newObj
 }
+
+

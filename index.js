a4e43e437f27f7f91f23f97df973bef8446ac827
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-10 17:14:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function deleteFromObjectByKey(object, key, value) {
   delete newObj [key]
   return newObj
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  return newObj
+}
